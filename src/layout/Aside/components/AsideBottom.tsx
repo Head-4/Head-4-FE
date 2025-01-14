1b7b5ff542b56a6c291fd105a87f8c50ff46a009
Hina0222 <hina0222@naver.com>
2025-01-14 18:59:28 +0900

    feat: KeyWordOnOffButton add

--- src/layout/Aside/components/AsideBottom.tsx
+++ src/layout/Aside/components/AsideBottom.tsx
@@ -4,6 +4,8 @@ import {ReactComponent as UniversityIcon} from "../../../assets/Aside/University
 import {ReactComponent as KeywordIcon} from "../../../assets/Aside/KeywordIcon.svg";
 import {ReactComponent as MessageIcon} from "../../../assets/Aside/MessageIcon.svg";
 import {ReactComponent as ExpandIcon} from "../../../assets/Aside/ExpandIcon.svg";
+import {ReactComponent as BellIcon} from "../../../assets/Aside/BellIcon.svg";
+import {useState} from "react";
 
 interface AsideBottomProps {
     toggleAside: () => void;
@@ -28,23 +30,70 @@ const AsideItems = [
 ];
 
 export default function AsideBottom({toggleAside}: AsideBottomProps) {
+    const [keyWordToggle, setKeyWordToggle] = useState<boolean>(false);
+    const clickKeyWordToggle = () => {
+        setKeyWordToggle((prev) => !prev);
+    };
+
     return (
-        <AsideBottomUl>
-            {AsideItems.map(({to, Icon, label}, index) => (
-                <li key={index}>
-                    <AsideLink to={to} onClick={toggleAside}>
-                        <Icon/>
-                        <AsideSetting>{label}</AsideSetting>
-                        <ExpandIcon/>
-                    </AsideLink>
-                </li>
-            ))}
-        </AsideBottomUl>
+        <>
+            <OnOffDiv>
+                <BellIcon/>
+                <AsideSetting>키워드 알림</AsideSetting>
+                <KeyWordOnOffButton onClick={clickKeyWordToggle} $keyWordToggle={keyWordToggle}>
+                    {keyWordToggle ? "ON" : "OFF"}
+                </KeyWordOnOffButton>
+            </OnOffDiv>
+            <ul>
+                {AsideItems.map(({to, Icon, label}, index) => (
+                    <li key={index}>
+                        <AsideLink to={to} onClick={toggleAside}>
+                            <Icon/>
+                            <AsideSetting>{label}</AsideSetting>
+                            <ExpandIcon/>
+                        </AsideLink>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
-const AsideBottomUl = styled.ul`
+const OnOffDiv = styled.div`
     margin-top: 10px;
+    display: flex;
+    column-gap: 12px;
+    padding: 16px 20px;
+    align-items: center;
+    color: #DDDDDD;
+`;
+
+const KeyWordOnOffButton = styled.button<{ $keyWordToggle: boolean }>`
+    padding: 0 10px;
+    text-align: ${({$keyWordToggle}) => $keyWordToggle ? 'left' : 'right'};
+    font-size: 12px;
+    font-weight: 600;
+    color: ${({$keyWordToggle, theme}) => $keyWordToggle ? theme.colors.primary : '#B7B7B7'};
+    position: relative;
+    width: 64px;
+    height: 32px;
+    border-radius: 24px;
+    border: 1px solid ${({$keyWordToggle}) => $keyWordToggle ? '#BFCFE9' : '#E2E2E2'};
+    background-color: ${({$keyWordToggle}) => $keyWordToggle ? '#E4EBF5' : '#F7F7F7'};
+    transition: background-color 0.2s;
+
+    &::after {
+        position: absolute;
+        content: "";
+        left: ${({$keyWordToggle}) => $keyWordToggle ? '38px' : '5px'};
+        top: 50%;
+        transform: translateY(-50%);
+        width: 20px;
+        height: 20px;
+        background-color: ${({$keyWordToggle, theme}) => $keyWordToggle ? theme.colors.primary : '#D0D0D0'};
+        border-radius: 50%;
+        transition: all 0.2s;
+    }
 `;
 
 const AsideLink = styled(Link)`
