74a9bb201ff69d3c5623090d4161c55379925169
Hina0222 <hina0222@naver.com>
2025-01-24 00:03:00 +0900

    style: typo add

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,15 @@ export const theme = {
     colors: {
         primary: "#246AD2",
         mainFont: "#33363F",
-        backgroundWhite:"#FFFFFF",
-        lightGray : "#E9E9E9",
+        backgroundWhite: "#FFFFFF",
+        lightGray: "#E9E9E9",
     },
+
+    Black: '#33363F',
+    White: '#FFFFFF',
+
+    //Color
+    Blue: "#246AD2",
+
+    LightGray:"#E9E9E9",
 };
