e579d30511bce1a85dc9bede63c6291d2664a9f0
Hina0222 <hina0222@naver.com>
2025-01-23 20:09:05 +0900

    feat: pwa 수정

--- src/pages/utilPages/PWAInstallModal.tsx
+++ src/pages/utilPages/PWAInstallModal.tsx
@@ -1,26 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Overlay from "../../styles/Common/Overlay";
 import CommonButton from "../../components/CommonButton";
 import styled from "styled-components";
 import {ReactComponent as MainLogo} from "../../assets/Logo/MainLogo.svg";
 
 export default function PwaInstallModal() {
-    const [isInstallable, setIsInstallable] = useState<boolean>(false);
     const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
-    const [isIOS, setIsIOS] = useState<boolean>(false);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
-        const isDeviceIOS =
-            /iPad|iPhone|iPod/.test(window.navigator.userAgent) && !(window as any).MSStream;
-        setIsIOS(isDeviceIOS);
-
         const handleBeforeInstallPrompt = (e: any) => {
-            // 기본 이벤트 방지
             e.preventDefault();
             setDeferredPrompt(e);
-            setIsInstallable(true);
+            setShowModal(true);
         };
-        // `beforeinstallprompt` 이벤트 리스너 등록
+
         window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
         return () => {
@@ -32,44 +26,37 @@ export default function PwaInstallModal() {
         if (!deferredPrompt) return;
 
         deferredPrompt.prompt();
-
         const choiceResult = await deferredPrompt.userChoice;
+
         if (choiceResult.outcome === "accepted") {
-            console.log("사용자가 PWA를 설치했습니다!");
+            console.log("PWA 설치 완료!");
         } else {
-            console.log("사용자가 PWA 설치를 취소했습니다.");
+            console.log("PWA 설치 취소");
         }
 
-        // 설치 프로세스 완료 후 초기화
         setDeferredPrompt(null);
-        setIsInstallable(false);
+        setShowModal(false);
     };
 
     const closeModal = () => {
-        setIsInstallable(false);
+        setShowModal(false);
         setDeferredPrompt(null);
     };
 
-    if (!isIOS && !isInstallable) {
-        return null;
-    }
+    if (!showModal) return null;
 
     return (
         <>
-            {isInstallable &&
-                <>
-                    <Overlay onClick={closeModal}/>
-                    <PWAModalDiv>
-                        <StyledMainLogo/>
-                        <PWAModalMSG>
-                            터치 한 번으로<br/>바로 시작해 보세요!
-                        </PWAModalMSG>
-                        <CommonButton isActive={true} onClick={installPWA}>
-                            앱 내려받기
-                        </CommonButton>
-                    </PWAModalDiv>
-                </>
-            }
+            <Overlay onClick={closeModal}/>
+            <PWAModalDiv>
+                <StyledMainLogo/>
+                <PWAModalMSG>
+                    터치 한 번으로<br/>바로 시작해 보세요!
+                </PWAModalMSG>
+                <CommonButton isActive={true} onClick={installPWA}>
+                    앱 내려받기
+                </CommonButton>
+            </PWAModalDiv>
         </>
     );
 }
